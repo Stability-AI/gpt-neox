59690c8bd17db3fe3371e35404693da9c9a8b6f9
Paul McCann <paul.mccann@stability.ai>
2023-06-06 14:21:06 +0900

    Fix comment

--- tools/preprocess_data.py
+++ tools/preprocess_data.py
@@ -76,7 +76,7 @@ def get_args():
         "--jsonl-keys",
         nargs="+",
         default=["text"],
-        help="space separate listed of keys to extract from jsonl. Defa",
+        help="space separate listed of keys to extract from jsonl. Default is `text`",
     )
     group.add_argument(
         "--num-docs",
